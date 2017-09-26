704b9499a67506983edbe4886bb781e5591cd114
Sergey Sidorov <sergey.ivanovich.sidorov@gmail.com>
2017-09-26 13:54:52 +0300

    changing account info, changing password

--- controllers/mainController.js
+++ controllers/mainController.js
@@ -8,7 +8,7 @@ mainApp.controller("mainController", function($scope)
         var token = localStorage.getItem("token");
         
         $.ajax({
-            url: "http://safemooney.azurewebsites.net/api/"+id+"/account/logout",
+            url: host + "/api/" + id + "/account/logout",
             type: 'GET',
             dataType: 'json',
             beforeSend: function (xhr, settings)
@@ -32,4 +32,74 @@ mainApp.controller("mainController", function($scope)
             }
         });
     }
+    $scope.account = {
+        userdata : {
+            firstname: "Sergey",
+            lastname: "Sidorov",
+            username: "SergeyIS",
+            oldpassword : "",
+            newpassword: ""
+        }
+    };
+
+    $scope.changeUserInfo = function(){
+        
+        var id = localStorage.getItem("userId");
+        var token = localStorage.getItem("token");
+
+        $.ajax({
+            url: host + "/api/" + id + "/account/changeuserinfo",
+            type: 'POST',
+            dataType: 'json',
+            data: $scope.account.userdata,
+            beforeSend: function (xhr, settings)
+            {
+                xhr.setRequestHeader('Authorization', 'Basic ' + token);
+            },
+            success: function (data)
+            {
+                localStorage.setItem("username", data["Username"]);
+                localStorage.setItem("firstName", data["FirstName"]);
+                localStorage.setItem("lastName", data["LastName"]);
+
+               alert("User's data was changed");
+            },
+            error: function (jqXHR, textStatus, errorThrown)
+            {
+                alert("There's some problem with your data or connection. Server returned status code: " + jqXHR.status);
+            }
+        });
+    };
+
+    $scope.changePassword = function(){
+        var id = localStorage.getItem("userId");
+        var token = localStorage.getItem("token");
+
+        $.ajax({
+            url: host + "/api/" + id + "/account/changepass",
+            type: 'POST',
+            dataType: 'json',
+            data: {oldpassword: $scope.account.userdata.oldpassword, newpassword: $scope.account.userdata.newpassword},
+            beforeSend: function (xhr, settings)
+            {
+                xhr.setRequestHeader('Authorization', 'Basic ' + token);
+            },
+            success: function (data)
+            {
+               alert("User's data was changed");
+            },
+            error: function (jqXHR, textStatus, errorThrown)
+            {
+                if(jqXHR.status != 200)
+                {
+                    alert("There's some problem with your data or connection. Server returned status code: " + jqXHR.status);
+                }
+                else
+                {
+                    alert("User's data was changed");
+                }
+            }
+        });
+    }
+    
 });
